6b72b6ba7af317326d66b52e0235294ed3970430
yuvrajanalyst <uvrushty@gmail.com>
2022-09-08 14:20:08 +0530

    first commit

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ def index():
 
     else:
         return render_template('index.html')
-
+#pip install -r requirements.txt ,
+#pip install -r requirements.txt
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
 	app.run(debug=True)
